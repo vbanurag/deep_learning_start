a92f0d59b8d8b956fa281d289d16e3fe8caa7f25
Anurag Sharma <anuragsharma@anurags-MacBook-Pro.local>
2018-02-11 20:04:23 +0530

    initial commit

---
+++ ann01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 11 19:06:28 2018
+
+@author: anuragsharma
+"""
+
+# installing theano
+
+#installing tensorflow
+
+#installing keras
+
